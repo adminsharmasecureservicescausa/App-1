153ba99392939f1300b72a051651fd4791829449
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-08-21 15:06:25 +0200

    feat(page load scroll): disable scroll to top in specific pages

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,7 +1,32 @@
 export default function (to, from, savedPosition) {
   if (savedPosition) {
     return savedPosition
-  } else {
-    return { x: 0, y: 0 }
   }
+
+  // If on special path, dont scroll to top
+  if (isOnSpecialPath(to.path)) {
+    return null
+  }
+
+  return { x: 0, y: 0 }
+}
+
+function isOnSpecialPath(path) {
+  let isSpecialPage = false
+
+  switch (path) {
+    //
+
+    // Content is appended, dont scroll
+    case '/':
+    case '/premium/saved-posts':
+      isSpecialPage = true
+      break
+
+    default:
+      isSpecialPage = false
+      break
+  }
+
+  return isSpecialPage
 }
